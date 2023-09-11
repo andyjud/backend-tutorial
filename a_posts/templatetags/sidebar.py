cd1e03d8e391bac91f2d1e5744d6a07610a1584f
Andreas Jud <andreas.jud@gmail.com>
2023-09-11 20:11:11 +0800

    Update sidebar.py

--- a_posts/templatetags/sidebar.py
+++ a_posts/templatetags/sidebar.py
@@ -1,16 +1,19 @@
 from django.template import Library
-from ..models import Tag, Post
+from ..models import Tag, Post, Comment
 from django.db.models import Count
 
 register = Library()
 
 @register.inclusion_tag('includes/sidebar.html')
-def sidebar_view(tag=None):
+def sidebar_view(tag=None, user=None):
     categories = Tag.objects.all()
     top_posts = Post.objects.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+    top_comments = Comment.objects.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
     context = {
         'categories': categories,
         'tag': tag,
-        'top_posts': top_posts
+        'top_posts': top_posts,
+        'top_comments': top_comments,
+        'user': user
     }
     return context
