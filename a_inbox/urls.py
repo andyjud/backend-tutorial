9bf5908bab22dff08b6141ea01e7a55f8d031ea3
Andreas Jud <andreas.jud@gmail.com>
2023-09-30 09:27:22 +0800

    urls

--- a_inbox/urls.py
+++ a_inbox/urls.py
@@ -4,9 +4,4 @@ from .views import *
 urlpatterns = [
     path('', inbox_view, name="inbox"), 
     path('c/<conversation_id>/', inbox_view, name="inbox"), 
-    path('search_users/', search_users, name="inbox-searchusers"),
-    path('new_message/<recipient_id>', new_message, name="inbox-newmessage"),
-    path('new_reply/<conversation_id>/', new_reply, name="inbox-newreply"),
-    path('notify/<conversation_id>/', notify_newmessage, name="notify-newmessage"),
-    path('notify-inbox/', notify_inbox, name="notify-inbox"),
 ]
