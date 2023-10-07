aeb765b37bad19f8e9168e2c023c0fdc54ff939b
Andreas Jud <andreas.jud@gmail.com>
2023-10-07 12:36:02 +0800

    #22

--- a_inbox/views.py
+++ a_inbox/views.py
@@ -12,6 +12,10 @@ def inbox_view(request, conversation_id=None):
     my_conversations = Conversation.objects.filter(participants=request.user)
     if conversation_id:
         conversation = get_object_or_404(my_conversations, id=conversation_id)
+        latest_message = conversation.messages.first()
+        if conversation.is_seen == False and latest_message.sender != request.user:
+            conversation.is_seen = True
+            conversation.save()
     else:
         conversation = None
     context = {
@@ -94,3 +98,22 @@ def new_reply(request, conversation_id):
         'conversation' : conversation
     }
     return render(request, 'a_inbox/form_newreply.html', context)
+
+
+
+def notify_newmessage(request, conversation_id):
+    conversation = get_object_or_404(Conversation, id=conversation_id)
+    latest_message = conversation.messages.first()
+    if conversation.is_seen == False and latest_message.sender != request.user:
+        return render(request, 'a_inbox/notify_icon.html')
+    else:
+        return HttpResponse('') 
+    
+    
+def notify_inbox(request):
+    my_conversations = Conversation.objects.filter(participants=request.user, is_seen=False)
+    for c in my_conversations:
+        latest_message = c.messages.first()
+        if latest_message.sender != request.user:
+            return render(request, 'a_inbox/notify_icon.html')
+    return HttpResponse('') 
