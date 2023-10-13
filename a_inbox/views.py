085aed91c3ca292a213a972b3272cffc130087ce
Andreas Jud <andreas.jud@gmail.com>
2023-10-13 17:49:42 +0800

    part 23

--- a_inbox/views.py
+++ a_inbox/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 from django.db.models import Q
 from a_users.models import Profile
+from cryptography.fernet import Fernet
+from django.conf import settings
 from .forms import InboxNewMessageForm
 from .models import *
 
+f = Fernet(settings.ENCRYPT_KEY)
 
 @login_required
 def inbox_view(request, conversation_id=None):
@@ -50,6 +53,14 @@ def new_message(request, recipient_id):
         form = InboxNewMessageForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
+
+            # encrypt message
+            message_original = form.cleaned_data['body']
+            message_bytes = message_original.encode('utf-8')
+            message_encrypted = f.encrypt(message_bytes)
+            message_decoded = message_encrypted.decode('utf-8')
+            message.body = message_decoded
+            
             message.sender = request.user
             
             my_conversations = request.user.conversations.all()
@@ -85,6 +96,14 @@ def new_reply(request, conversation_id):
         form = InboxNewMessageForm(request.POST)
         if form.is_valid():
             message = form.save(commit=False)
+
+            # encrypt message
+            message_original = form.cleaned_data['body']
+            message_bytes = message_original.encode('utf-8')
+            message_encrypted = f.encrypt(message_bytes)
+            message_decoded = message_encrypted.decode('utf-8')
+            message.body = message_decoded
+            
             message.sender = request.user
             message.conversation = conversation
             message.save()
