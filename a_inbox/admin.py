100a5260485e185ab791a061fb49f2d381d7f095
Andreas Jud <andreas.jud@gmail.com>
2023-10-13 17:45:37 +0800

    part 23

--- a_inbox/admin.py
+++ a_inbox/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import *
 
-admin.site.register(InboxMessage)
+class InboxMessageAdmin(admin.ModelAdmin):
+    readonly_fields = ('sender', 'conversation', 'body')
+
+admin.site.register(InboxMessage, InboxMessageAdmin)
 admin.site.register(Conversation)
