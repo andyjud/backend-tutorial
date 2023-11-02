b74f9af9326d9d87f375ce416450b34475ab3570
Andreas Jud <andreas.jud@gmail.com>
2023-11-02 13:30:53 +0800

    Add files via upload

---
+++ node/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../templates/**/*.html',
+    '../**/templates/**/*.html',
+    '../**/forms.py'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
