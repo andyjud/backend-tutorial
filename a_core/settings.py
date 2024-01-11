30df53f4717dc7deaf2548ee5c8e2b3dab3f4ac3
Andreas Jud <andreas.jud@gmail.com>
2024-01-11 13:15:25 +0800

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -40,7 +40,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', env('RENDER_EXTERNAL_HOSTNAME')]
+
+CSRF_TRUSTED_ORIGINS = [ 'https://*.onrender.com' ]
 
 INTERNAL_IPS = (
     '127.0.0.1',
