5236e9b9fb5447f3d523139ef901edbdd39d49f8
Andreas Jud <andreas.jud@gmail.com>
2024-01-24 16:52:45 +0800

    Update urls.py

--- a_core/urls.py
+++ a_core/urls.py
@@ -20,8 +20,21 @@ from a_posts.views import *
 from a_users.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
+
+# Sitemaps
+from django.contrib.sitemaps.views import sitemap
+from a_posts.sitemaps import *
+
+sitemaps = {
+    'static' : StaticSitemap,
+    'categories' : CategorySitemap,
+    'postpages' : PostpageSitemap
+}
 
 urlpatterns = [
+    path('sitemap.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('theboss/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
